837682a5b44323bbfb775130ac3cf610e662a5d6
jiashuomeng <mengjiashuo@youzan.com>
2018-05-03 17:09:47 +0800

    2018年 5月 3日 星期四 17时09分47秒 CST

--- push.sh
+++ push.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-git add .
-
-git commit -m "`date`"
-
-git push origin `git_branch`
 
 function git_branch {
    branch="`git branch 2>/dev/null | grep "^\*" | sed -e "s/^\*\ //"`"
@@ -15,4 +10,10 @@ function git_branch {
        echo $branch
    fi
 }
+
+git add .
+
+git commit -m "`date`"
+
+git push origin `git_branch`
 
