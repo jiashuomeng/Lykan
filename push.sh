d9d33be4ce1ada459a4c610500ab32aa9e4f3e4b
jiashuomeng <mengjiashuo@youzan.com>
2018-05-03 17:09:12 +0800

    2018年 5月 3日 星期四 17时09分12秒 CST

--- push.sh
+++ push.sh
@@ -4,5 +4,15 @@ git add .
 
 git commit -m "`date`"
 
-git push origin master
+git push origin `git_branch`
+
+function git_branch {
+   branch="`git branch 2>/dev/null | grep "^\*" | sed -e "s/^\*\ //"`"
+   if [ "${branch}" != "" ];then
+       if [ "${branch}" = "(no branch)" ];then
+           branch="(`git rev-parse --short HEAD`...)"
+       fi
+       echo $branch
+   fi
+}
 
